f40a42097cdadc682a099d06bb8b42a3e914e6ea
AzulGrajales <azul.grajales35@my.unitec.edu.mx>
2023-09-30 22:29:25 -0600

    Creado hasta ahora

---
+++ TheMissionGame/Assets/Scripts/ControlJugPlat.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugPlat : MonoBehaviour
+{
+    private Rigidbody2D rbJugador;
+    private Animator animJugador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbJugador = GetComponent<Rigidbody2D>();
+        animJugador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.D))
+        {
+            animJugador.SetBool("esCaminando", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animJugador.SetBool("esCaminando", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animJugador.SetBool("esCaminando", true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animJugador.SetBool("esCaminando", false);
+        }
+    }
+}
