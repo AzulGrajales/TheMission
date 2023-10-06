bb869c712540a14d899f6fcf75e0dd0176c9a4c1
CinnamonPretzel <newtonesunvidis@gmail.com>
2023-10-05 22:49:56 -0600

    ofvnlovm

--- TheMissionGame/Assets/Scripts/ControlJugPlat.cs
+++ TheMissionGame/Assets/Scripts/ControlJugPlat.cs
@@ -7,6 +7,14 @@ public class ControlJugPlat : MonoBehaviour
     private Rigidbody2D rbJugador;
     private Animator animJugador;
 
+    //para caminar
+    private bool caminar;
+    private float speed = 3.5f;
+
+    //salto
+    public LayerMask capaDelPiso;
+    public Transform sensorParaPiso;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +29,30 @@ public class ControlJugPlat : MonoBehaviour
         {
             animJugador.SetBool("esCaminando", true);
             transform.localRotation = Quaternion.Euler(0, 0, 0);
+            caminar = true;
+            speed = 3.5f;
         }
         if (Input.GetKeyUp(KeyCode.D))
         {
             animJugador.SetBool("esCaminando", false);
+            caminar = false;
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             animJugador.SetBool("esCaminando", true);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            caminar = true;
+            speed = -3.5f;
         }
         if (Input.GetKeyUp(KeyCode.A))
         {
             animJugador.SetBool("esCaminando", false);
+            caminar = false;
+        }
+
+        if (caminar)
+        {
+            rbJugador.velocity = new Vector2(speed, rbJugador.velocity.y);
         }
     }
 }
